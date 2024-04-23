ec723482b863f508908708f1b88cb6a2873b32c3
Giammarco <giammarcogaudenzi@gmail.com>
2024-04-23 17:46:28 +0200

    sidebar menu animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+
 import Layout from "./components/Layout";
+import Homepage from "./pages/Homepage";
 
 const routerAlt = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<h1>Homepage</h1>} />
+      <Route index element={<Homepage />} />
       <Route path="/about/" element={<h1>About page</h1>} />
       <Route path="/contacts/" element={<h1>Contacts page</h1>} />
     </Route>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,13 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Outlet, Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div style={{ border: "2px solid red" }}>
+    <div>
       <Menu />
-      <div style={{ border: "8px solid blue" }}>
+      <div className="page">
         <Outlet />
       </div>
     </div>
@@ -13,12 +16,82 @@ const Layout = () => {
 
 export default Layout;
 
+const menuItems = [
+  {
+    path: "/",
+    label: "Homepage",
+  },
+  {
+    path: "/about",
+    label: "About",
+  },
+  {
+    path: "/contacts",
+    label: "Contacts",
+  },
+];
+
 const Menu = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    // Here you run the code for the side effects
+    setIsVisible(false);
+  }, [location]); // dependencies array
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contacts">Contacts</Link>
-    </div>
+    <>
+      <button onClick={() => setIsVisible(!isVisible)} className="menu-toggle">
+        {isVisible ? "hide" : "show"}
+      </button>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            className="backdrop"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 0.6,
+            }}
+            exit={{
+              opacity: 0,
+            }}
+            transition={{
+              delay: 0.2,
+            }}
+            onClick={() => setIsVisible(false)}
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            className="menu"
+            initial={{
+              transform: "translateX(-100%)",
+            }}
+            animate={{
+              transform: "translateX(0%)",
+            }}
+            exit={{
+              transform: "translateX(-100%)",
+            }}
+          >
+            <ul>
+              {menuItems.map((item, index) => {
+                return (
+                  <motion.li key={index}>
+                    <Link to={item.path}>{item.label}</Link>
+                  </motion.li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
